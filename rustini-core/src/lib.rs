ec7f29c4b8efa31fa3c14245ae6e84eedd0ec31b
dablenparty <hdavenport8902@gmail.com>
2023-09-23 14:25:40 -0400

    Created IniStruct trait

--- rustini-core/src/lib.rs
+++ rustini-core/src/lib.rs
@@ -1,1 +1,17 @@
 #![warn(clippy::all, clippy::pedantic)]
+
+pub use anyhow;
+pub use thiserror;
+
+/// Trait for converting between an INI file and a struct.
+pub trait IniStruct<T> {
+    type Error;
+
+    /// Convert an INI string to a struct.
+    fn from_ini<S>(ini: S) -> Result<T, Self::Error>
+    where
+        S: AsRef<str>;
+
+    /// Convert this struct to an INI string.
+    fn to_ini(&self) -> String;
+}
