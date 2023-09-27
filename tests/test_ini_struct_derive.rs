3cf74960443ce6dc05bcd0c218ffde0cf94ba8d6
dablenparty <hdavenport8902@gmail.com>
2023-09-27 14:56:55 -0400

    Created tests for optional fields

--- tests/test_ini_struct_derive.rs
+++ tests/test_ini_struct_derive.rs
@@ -8,6 +8,13 @@ struct ConvertMe {
     c: u32,
 }
 
+#[derive(IniStruct, PartialEq, Eq, Debug)]
+struct OptionalConvertMe {
+    a: String,
+    b: Option<bool>,
+    c: Option<u32>,
+}
+
 #[test]
 fn test_derive_from_ini() -> anyhow::Result<()> {
     let ini_str = r#"
@@ -26,6 +33,22 @@ fn test_derive_from_ini() -> anyhow::Result<()> {
 }
 
 #[test]
+fn test_derive_optional_from_ini() -> anyhow::Result<()> {
+    let ini_str = r#"
+        a = Hello, world!
+        c = 42
+    "#;
+    let data = OptionalConvertMe::from_ini(ini_str)?;
+    let expected = OptionalConvertMe {
+        a: "Hello, world!".to_string(),
+        b: None,
+        c: Some(42),
+    };
+    assert_eq!(data, expected);
+    Ok(())
+}
+
+#[test]
 fn test_derive_to_ini() -> anyhow::Result<()> {
     let data = ConvertMe {
         a: "Hello, world!".to_string(),
@@ -36,5 +59,23 @@ fn test_derive_to_ini() -> anyhow::Result<()> {
     // raw strings add a lot of extra space, this is a workaround
     let expected = ["a = Hello, world!", "b = true", "c = 42"].join("\n");
     assert_eq!(ini_str, expected);
+    Ok(())
+}
+
+#[test]
+fn test_derive_optional_to_ini() -> anyhow::Result<()> {
+    let data = OptionalConvertMe {
+        a: "Hello, world!".to_string(),
+        b: None,
+        c: Some(42),
+    };
+    // TODO: preserve order of fields
+    let ini_str = data.to_ini();
+    let mut ini_lines = ini_str.lines().collect::<Vec<_>>();
+    // since field names are at the start of a line, we can sort by them easily
+    ini_lines.sort();
+    // raw strings add a lot of extra spaces, this is a workaround
+    let expected = ["a = Hello, world!", "c = 42"];
+    assert_eq!(ini_lines, expected);
     Ok(())
 }
